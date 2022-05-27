dcc9730017557aea33c483a101c51e074fef9e9c
Renato Henrique <renato.henrique_98@hotmail.com>
2022-05-27 15:52:46 -0300

    chore: create project

---
+++ src/main/java/com/github/throyer/App.java
@@ -0,0 +1,9 @@
+package com.github.throyer;
+
+import static java.lang.System.out;
+
+public class App {
+    public static void main(String... args) {
+        out.println("Hello World!");
+    }
+}
