c2baace5ea17b0892271a65a92f214f543deb1f3
Renato Henrique <renato.henrique_98@hotmail.com>
2022-05-27 15:57:50 -0300

    chore: add driver

--- src/main/java/com/github/throyer/App.java
+++ src/main/java/com/github/throyer/App.java
@@ -1,9 +1,14 @@
 package com.github.throyer;
 
-import static java.lang.System.out;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
     public static void main(String... args) {
-        out.println("Hello World!");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        WebDriver browser = new ChromeDriver();
+        browser.navigate().to("http://google.com.br");
+        browser.quit();
     }
 }
